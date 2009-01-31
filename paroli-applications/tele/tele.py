f08a4666e156fc400f2567c093347ff1122f9744
Mirko Lindner <mirko@mira.(none)>
2009-01-31 14:58:31 +0100

    catch delete requests to apps

--- paroli-applications/tele/tele.py
+++ paroli-applications/tele/tele.py
@@ -36,12 +36,12 @@ class DialerApp(tichy.Application):
         logger.info("loading")
         self.standalone = tichy.Text.as_type(text)
         self.main = parent
-        
+
         ##set title if not in launcher mode
         if self.main.etk_obj.title_get() != 'Home':
             self.main.etk_obj.title_set('Tele')
-            #self.main.etk_obj.show()
-
+            self.main.etk_obj.show()
+        
         ##set edje_file
         self.edje_file = os.path.join(os.path.dirname(__file__),'tele.edj')
 
@@ -60,7 +60,9 @@ class DialerApp(tichy.Application):
         
         if self.standalone == 1:
             self.edje_obj.Edje.size_set(480,550)
-        self.edje_obj.Edje.pos_set(0,30)
+            self.edje_obj.Edje.pos_set(0,30)
+        else:
+            self.edje_obj.Edje.size_set(480,590)
         self.edje_obj.Edje.name_set('main_tele_window')
         self.edje_obj.show()
         self.main.connect('hide_Tele',self.edje_obj.hide)
@@ -70,18 +72,22 @@ class DialerApp(tichy.Application):
         
         self.edje_obj.add_callback("*", "embryo", self.embryo)
         self.edje_obj.add_callback("*", "call", self.call)
+        
         ##wait until main object emits back signal
-        yield tichy.Wait(self.main, 'back_Tele')
+        yield tichy.Wait(self.main, 'delete_request')
         logger.info('Tele closing')
         ##remove all children -- edje elements if not in launcher mode
         if self.standalone == 1:
+            print "delete"
             self.edje_obj.delete()
-            #for i in self.edje_obj.data['windows']:
-                #i.delete(None,None,None)
+            
         else:    
-            for i in self.main.children:
-                i.remove()
+            self.edje_obj.delete()
             self.main.etk_obj.hide()   # Don't forget to close the window
+
+    def delete_request(self, *args, **kargs):
+        self.main.emit('back_Tele')
+        logger.info('delete_request' + str(args))
 
     def embryo(self, emission, signal, source):
         logger.info("embryo says:" + str(signal))
