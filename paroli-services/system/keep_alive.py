a69e49167d9a2b8c6712c9d17eb3c702119ef256
Guillaume Chereau <charlie@openmoko.org>
2009-02-02 19:29:21 +0800

    add the keep_alive service service
    I put the service into a sub directory system.
    It could be used to maintain the framework daemon running even after a crash.

---
+++ paroli-services/system/keep_alive.py
@@ -0,0 +1,57 @@
+#
+#    Paroli
+#
+#    copyright 2008 openmoko
+#
+#    This file is part of Paroli
+#
+#    Tichy is free software: you can redistribute it and/or modify it
+#    under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Paroli is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Paroli.  If not, see <http://www.gnu.org/licenses/>.
+
+
+__docformat__ = 'reStructuredText'
+
+import logging
+LOGGER = logging.getLogger('keep_alive')
+
+from subprocess import call, PIPE
+import os
+
+import tichy
+
+class KeepAliveService(tichy.Service):
+    """This service can be used to make sure that a given daemon is
+    alive and restart it if it is dead.
+
+    The implementation is pretty bad, using subprocess to directly
+    call the pgrep unix command.
+
+    (Is there a better way to do that ?)
+    """
+    service = 'KeepAlive'
+
+    @tichy.tasklet.tasklet
+    def keep_alive(self, daemon, period=5):
+        """Tasklet that will ensure that the daemon stay alive"""
+        # We make sure that the init.d script is present
+        init_script = "/etc/init.d/%s" % daemon
+        if not os.path.exists(init_script):
+            LOGGER.error('cannot find init script "%s"', init_script)
+            raise Exception('file "%s" not present' % init_script)
+        while True:
+            is_alive = call(['pgrep', '-f', daemon], stdout=PIPE) == 0
+            if not is_alive:
+                LOGGER.error("daemon %s is dead", daemon)
+                LOGGER.error("restarting %s", daemon)
+                call([init_script, 'start'])
+            yield tichy.tasklet.Sleep(period)
---
+++ paroli-services/system/system.py
@@ -0,0 +1,3 @@
+
+
+import keep_alive
