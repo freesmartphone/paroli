edb71d6b3a3410f4b4d5e31713bbfc4d69166b0c
Guillaume Chereau <charlie@openmoko.org>
2009-02-10 17:55:55 +0800

    audio: minor change

--- paroli-services/hardware/audio.py
+++ paroli-services/hardware/audio.py
@@ -15,7 +15,7 @@
 #    General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with Tichy.  If not, see <http://www.gnu.org/licenses/>.
+#    along with paroli.  If not, see <http://www.gnu.org/licenses/>.
 
 import dbus
 
