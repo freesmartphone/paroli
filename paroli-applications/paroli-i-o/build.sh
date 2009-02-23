4d435a5008f6129d0788554d756dfdc88d39b48f
Jeremy Chang <jeremy@openmoko.com>
2009-02-23 16:02:54 +0800

    Use another font and image path for the edje of I/O.

--- paroli-applications/paroli-i-o/build.sh
+++ paroli-applications/paroli-i-o/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-edje_cc $@ -id edje/default/images -fd . edje/default/paroli-i-o.edc -o paroli-i-o.edj
+edje_cc $@ -id edje/default/images -id ../common-for-edje/images -fd ../common-for-edje/fonts edje/default/paroli-i-o.edc -o paroli-i-o.edj
 chmod +r paroli-i-o.edj
