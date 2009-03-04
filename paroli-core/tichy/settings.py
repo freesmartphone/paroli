45c159197e38e3887af71831a32d7d8550b5748f
Guillaume Chereau <charlie@openmoko.org>
2009-03-04 18:55:17 +0800

    settings: add the FSOSetting class
    This hasn't been tested at all yet

--- paroli-core/tichy/settings.py
+++ paroli-core/tichy/settings.py
@@ -104,6 +104,25 @@ class Setting(Object):
         return self.__value.disconnect(*args, **kargs)
 
 
+class FSOSetting(Setting):
+    """Special setting class that hooks into a FSO preference
+
+    It relies on the 'Prefs' service.
+    """
+    @property
+    def value(self):
+        """accessor to the actual value"""
+        prefs = tichy.Service.get('Prefs')
+        return prefs[self.group][self.name]
+
+    @tasklet.tasklet
+    def set(self, value):
+        """Try to set the Setting value and block until it is done"""
+        prefs = tichy.Service.get('Prefs')
+        # XXX: make this asynchronous
+        prefs[self.group][self.name] = value
+        yield None
+
 if __name__ == '__main__':
     # Simple usage example
 
