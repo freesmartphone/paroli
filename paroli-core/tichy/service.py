010f07ff3db4147705eb16d1147479e5ca84a48a
Guillaume Chereau <charlie@openmoko.org>
2009-03-09 16:34:32 +0800

    service: Use no timeout by default when we wait for a service to init
    Some services may interact with the user at init (like gsm to ask the PIN)

--- paroli-core/tichy/service.py
+++ paroli-core/tichy/service.py
@@ -114,7 +114,7 @@ class Service(Item):
             self.emit('_fail_initialize') # internal signal
 
     @tichy.tasklet.tasklet
-    def wait_initialized(self, timeout=600):
+    def wait_initialized(self, timeout=None):
         """Block until the service is initialized
 
         The method will raise an exception if the service fails to
