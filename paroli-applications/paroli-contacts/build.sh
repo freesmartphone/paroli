1c5087187cde20238ebbb7cb594259257f82b629
Guillaume Chereau <charlie@openmoko.org>
2009-03-10 18:36:17 +0800

    remove paroli-contact application
    It has been replaced by People

--- paroli-applications/paroli-contacts/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-edje_cc $@ -id edje/default/images -id ../common-for-edje/images -fd ../common-for-edje/fonts edje/default/paroli-contacts.edc -o paroli-contacts.edj
-chmod +r paroli-contacts.edj
