c758f001588725a76d754af2f0fdb6460d025b8d
Guillaume Chereau <charlie@openmoko.org>
2009-03-10 18:33:47 +0800

    remove paroli-dialer application
    It has been replaced by tele

--- paroli-applications/build.sh
+++ paroli-applications/build.sh
@@ -2,7 +2,6 @@
 
 # This script will build the edje files for each application
 cd paroli-contacts; ./build.sh; cd ..
-cd paroli-dialer; ./build.sh; cd ..
 cd paroli-i-o; ./build.sh; cd ..
 cd paroli-launcher; ./build.sh; cd ..
 cd tele; ./build.sh; cd ..
