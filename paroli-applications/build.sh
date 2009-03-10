15daaf8cb8744d2e3d3a9f9697b0ada4ee337640
Guillaume Chereau <charlie@openmoko.org>
2009-03-10 18:31:05 +0800

    remove paroli-msgs
    This application has been replaced by msgs.

--- paroli-applications/build.sh
+++ paroli-applications/build.sh
@@ -4,7 +4,6 @@
 cd paroli-contacts; ./build.sh; cd ..
 cd paroli-dialer; ./build.sh; cd ..
 cd paroli-i-o; ./build.sh; cd ..
-cd paroli-msgs; ./build.sh; cd ..
 cd paroli-launcher; ./build.sh; cd ..
 cd tele; ./build.sh; cd ..
 cd msgs; ./build.sh; cd ..
