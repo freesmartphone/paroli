a88d59fa4ad6ac52d098ae89b4b067026b3b767b
Guillaume Chereau <charlie@openmoko.org>
2009-03-13 16:16:20 +0800

    tel_number: remove some dead code

--- paroli-services/phone/tel_number.py
+++ paroli-services/phone/tel_number.py
@@ -55,9 +55,6 @@ class TelNumber(tichy.Text):
         contacts_service = tichy.Service.get('Contacts')
         return contacts_service.get_by_number(self.value)
 
-    def input_method(self):
-        return 'number'
-
     def get_text(self):
         self.update_view_text()
         return self.view_text
@@ -70,20 +67,3 @@ class TelNumber(tichy.Text):
             self.view_text.value = contact.get_text()
         else:
             self.view_text.value = self.value
-
-    def edit(self, window, name='number', **kargs):
-        text_edit = tichy.Service.get('TextEdit')
-        return text_edit.edit(window, self, input_method='number',
-                              name=name, **kargs)
-
-    def create_actor(self):
-        ret = super(TelNumber, self).create_actor()
-
-        def on_contact(actor, item, view):
-            select_contact = tichy.Service.get('SelectContact')
-            contact = yield select_contact.select(view.window)
-            if contact and contact.tel:
-                item.value = str(contact.tel)
-        ret.new_action("Contact").connect('activated', on_contact)
-
-        return ret
