ffa0b2355f797946c1d44d76f9c9db5c5bf06e94
Mirko <mirko@openmoko.org>
2009-03-28 17:42:08 -0400

    switch to tele2

--- paroli-applications/build.sh
+++ paroli-applications/build.sh
@@ -3,7 +3,7 @@
 # This script will build the edje files for each application
 cd i-o2; ./build.sh; cd ..
 cd paroli-launcher; ./build.sh; cd ..
-cd tele; ./build.sh; cd ..
+cd tele2; ./build.sh; cd ..
 cd msgs2; ./build.sh; cd ..
 cd people2; ./build.sh; cd ..
 
