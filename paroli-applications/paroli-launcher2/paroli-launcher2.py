90eab8653a3e5048818989548c307d02c185c0bf
Mirko <mirko@openmoko.org>
2009-03-30 09:47:05 -0400

    disable graphics for profile switching

--- paroli-applications/paroli-launcher2/paroli-launcher2.py
+++ paroli-applications/paroli-launcher2/paroli-launcher2.py
@@ -350,11 +350,11 @@ class Launcher_App2(tichy.Application):
                 new = available[0]
             else:
                 new = available[current_index+1]
-            edje_obj = gui.EdjeObject(self.main, self.edje_file, 'profile')    
-            edje_obj.Edje.part_text_set('text',new)
-            edje_obj.Edje.signal_callback_add('erase', '*', edje_obj.delete)
-            edje_obj.Edje.size_set(480, 600)
-            edje_obj.show()
+            #edje_obj = gui.EdjeObject(self.main, self.edje_file, 'profile')    
+            #edje_obj.Edje.part_text_set('text',new)
+            #edje_obj.Edje.signal_callback_add('erase', '*', edje_obj.delete)
+            #edje_obj.Edje.size_set(480, 600)
+            #edje_obj.show()
             self.prefs.set_profile(new)
             
             logger.info("current: %s new: %s", current, new)
