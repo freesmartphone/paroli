b9d1b75140ad82b733aa37f65301a6051cfa2a7f
Laszlo <lama@lamagep.(none)>
2009-05-23 11:33:12 +0200

    When building .edjs be a bit more informative. Especially when a .edj fails to build, we can see where it failed (at which program's .edc)

--- paroli-applications/paroli-launcher2/build.sh
+++ paroli-applications/paroli-launcher2/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+
+echo "building paroli-launcher.edj..."
 edje_cc $@ -id edje/default/images -id ../common-for-edje/images -fd ../common-for-edje/fonts edje/default/paroli-launcher.edc -o paroli-launcher.edj
 chmod +r paroli-launcher.edj
--- paroli-applications/tele2/build.sh
+++ paroli-applications/tele2/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+echo "building tele.edj..."
 edje_cc $@ -id edje/default/images -id ../common-for-edje/images -fd ../common-for-edje/fonts edje/default/tele.edc -o tele.edj
 chmod +r tele.edj
+
