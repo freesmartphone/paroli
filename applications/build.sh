55c933317bba5d2008a6a32fad5acb6603d1af87
M. Dietrich <mdt@emdete.de>
2009-05-30 17:57:06 +0200

    make the all--app build script generic

--- applications/build.sh
+++ applications/build.sh
@@ -1,68 +1,7 @@
 #!/bin/sh -e
-
-flag_io=
-flag_launcher=
-flag_msgs=
-flag_people=
-flag_settings=
-flag_tele=
-
-while getopts 'ilmpst' OPTION
-do
-  case $OPTION in
-  i)	flag_io=1
-	;;
-  l)	flag_launcher=1
-	;;
-  m)	flag_msgs=1
-	;;
-  p)	flag_people=1
-	;;
-  s)	flag_settings=1
-	;;
-  t)	flag_tele=1
-	;;
-  ?)	printf "Usage: %s: [-i] [-l] [-m] [-p] [-s] [-t]\n" $(basename $0) >&2
-	exit 2
-	;;
-  esac
+for d in *; do
+	if [ -x $d/build.sh ] ; then
+		export d
+		(cd $d && ./build.sh)
+	fi
 done
-
-if [[ "$flag_io" -eq "0" && "$flag_launcher" -eq "0" && \
-       "$flag_msgs" -eq "0" && "$flag_people" -eq "0" && \
-       "$flag_settings" -eq "0" && "$flag_tele" -eq "0" ]]
-then
-	flag_io=1
-	flag_launcher=1
-	flag_msgs=1
-	flag_people=1
-	flag_settings=1
-	flag_tele=1
-fi
-
-# This script will build the edje files for each application
-if [ "$flag_io" ]
-then
-	cd inout; ./build.sh; cd ..
-fi
-if [ "$flag_launcher" ]
-then
-	cd launcher; ./build.sh; cd ..
-fi
-if [ "$flag_msgs" ]
-then
-	cd letters; ./build.sh; cd ..
-fi
-if [ "$flag_people" ]
-then
-	cd people; ./build.sh; cd ..
-fi
-if [ "$flag_settings" ]
-then
-	cd settings; ./build.sh; cd ..
-fi
-if [ "$flag_tele" ]
-then
-	cd telefony; ./build.sh; cd ..
-fi
-
