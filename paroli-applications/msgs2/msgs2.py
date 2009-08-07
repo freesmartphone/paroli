09f34ee0fec480f5f1cdb3f85e0b9de65261564f
Mirko Lindner <vegyraupe@paroli-project.org>
2009-08-07 12:08:10 +0200

    fixed ticket #141 - sending sms to empty number paroli now checks whether a number longer than 3 digits was entered and if not will show an error message for details check http://www.paroli-project.org/trac/ticket/141

--- paroli-applications/msgs2/msgs2.py
+++ paroli-applications/msgs2/msgs2.py
@@ -300,7 +300,7 @@ class MsgsWrite(tichy.Application):
               
                   textbox.on_key_down_add(self.counter, text_layout)
                   
-                  text_layout.connect("send_request", self.send_request, textbox)
+                  text_layout.connect("send_request", self.send_request, textbox, sms.peer)
               
               i, args = yield tichy.WaitFirst(Wait(text_layout, 'back'), Wait(text_layout, 'send'))
               if i == 0: #back
@@ -355,15 +355,18 @@ class MsgsWrite(tichy.Application):
         layout.Edje.part_text_set( "counter-text", str(len(counter)))
     
     #send_request
-    def send_request(self, layout, entry, **kargs):
+    def send_request(self, layout, entry, number, **kargs):
 
-        counter = unicode(entry.entry_get()).encode("utf-8").replace("<br>","").strip()
-        if len(counter) == 0:
-            self.send_empty(layout).start()
-        elif len(counter) > 159:
-            self.dialog.dialog("layout", "Error", "Text too long, only single messages possible at the moment, please shorten your text").start()
+        if len(number) < 4 :
+            self.dialog.dialog("layout", "Error", "Number too short or none entered. Please enter a valid number.").start()
         else:
-            layout.emit("send")
+            counter = unicode(entry.entry_get()).encode("utf-8").replace("<br>","").strip()
+            if len(counter) == 0:
+                self.send_empty(layout).start()
+            elif len(counter) > 159:
+                self.dialog.dialog("layout", "Error", "Text too long, only single messages possible at the moment, please shorten your text").start()
+            else:
+                layout.emit("send")
     
     @tichy.tasklet.tasklet
     def send_empty(self, layout, **kargs):
