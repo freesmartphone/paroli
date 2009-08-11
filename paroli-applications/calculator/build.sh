b705b53b3487666e1896f7b8cec60847b6d587e9
Volker Mische <volker.mische@gmail.com>
2009-08-11 18:10:21 +0200

    Removed old calculator directories, they are no longer needed.

--- paroli-applications/calculator/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "building calculator.edj..."
-edje_cc $@ -id edje/default/images -id ../common-for-edje/images -fd ../common-for-edje/fonts edje/default/calculator.edc -o calculator.edj
-chmod +r calculator.edj
-
