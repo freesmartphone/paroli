af1814fa8199201afb258404c9b02d2cc3ff1d03
Angus Ainslie <nytowl@openmoko.org>
2009-08-30 23:00:10 -0600

    setup.py : use correct path for calculator

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(name='Paroli',
           + plugins_files('paroli-applications/people2', '../../usr/share/paroli/applications/people2') \
           + plugins_files('paroli-applications/settings', '../../usr/share/paroli/applications/settings') \
           + plugins_files('paroli-applications/tele2', '../../usr/share/paroli/applications/tele2') \
-          + plugins_files('paroli-applications/calculator', '../../usr/share/paroli/paroli-applications/calculator') \
+          + plugins_files('paroli-applications/calculator', '../../usr/share/paroli/applications/calculator') \
           + dbus_data \
           + sound_data,
 
